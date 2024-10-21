b0a405e6e6b8194df46aec32167d584338a9c11d
chenweiqi2 <chenweiqi2@tal.com>
2024-10-21 15:48:23 +0800

    更新版本号

--- upload.sh
+++ upload.sh
@@ -2,21 +2,21 @@
 #准备上传的类库 module
 upArray=(
   # 核心基础库
-  ":CoreFramework"
+#  ":CoreFramework"
 
-#  # 网络库
+  # 网络库
 #  ":ApiHttpSDK:ApiHttpBase"
 #  ":ApiHttpSDK:ApiHttpInfo"
 
-#  # 线程库
+  # 线程库
 #  ":ThreadPoolSDK:ThreadPoolBase"
 #  ":ThreadPoolSDK:ThreadPoolInfo"
 
-#  # 图片库
+  # 图片库
 #  ":ImageLoaderSDK:ImageLoaderBase"
 #  ":ImageLoaderSDK:ImageLoaderInfo"
 
-#  # web 容器库
+  # web 容器库
 #  ":WebViewSDK:WebViewBase"
 #  ":WebViewSDK:WebViewInfo"
 #  ":WebViewSDK:WebViewBridgeDS"
