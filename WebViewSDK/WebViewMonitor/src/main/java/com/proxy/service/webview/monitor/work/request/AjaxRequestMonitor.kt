d79a5c1cf1a4234df85ae4c3810485107f7c23ad
chenweiqi2 <chenweiqi2@tal.com>
2026-01-26 14:51:30 +0800

    更新ajax请求日志打印规则

--- WebViewSDK/WebViewMonitor/src/main/java/com/proxy/service/webview/monitor/work/request/AjaxRequestMonitor.kt
+++ WebViewSDK/WebViewMonitor/src/main/java/com/proxy/service/webview/monitor/work/request/AjaxRequestMonitor.kt
@@ -15,6 +15,12 @@ object AjaxRequestMonitor : BaseMonitor() {
 
     private const val TAG = "${WebMonitorConstants.TAG}AjaxReq"
 
+    private const val TYPE_STREAM = "jpg|jpeg|png|gif|bmp|webp|svg|mp4|avi|mov|mkv|webm"
+    private const val TYPE_IMAGE = "content-type: image/"
+    private const val TYPE_VIDEO = "content-type: video/"
+
+    private const val MAX_LENGTH = 1000
+
     override fun shouldRun(): Boolean {
         val enableLog = config.isLogAjaxRequestEnable()
         val callback = config.getLogAjaxRequestCallback()
@@ -26,14 +32,46 @@ object AjaxRequestMonitor : BaseMonitor() {
         val js = "var originalOpen = XMLHttpRequest.prototype.open;" +
                 "XMLHttpRequest.prototype.open = function(method, url) {" +
                 "    this._startTime = Date.now();" +
+                "    this._isBinaryContent = false;" +
+                "    if (url.match(/\\.($TYPE_STREAM)\$/i)) {" +
+                "        this._isBinaryContent = true;" +
+                "    };" +
                 "    this.addEventListener('readystatechange', function() {" +
                 "        if (this.readyState == 4) {" +
                 "            var endTime = Date.now();" +
                 "            var duration = endTime - this._startTime;" +
-                "            var headers = this.getAllResponseHeaders();" +
-                "            var body = this.responseText;" +
+
                 "            var requestHeaders = JSON.stringify(this._requestHeaders || {});" +
+                "            if (requestHeaders.length > $MAX_LENGTH) {" +
+                "                requestHeaders = requestHeaders.substring(0, $MAX_LENGTH) + \" 超出${MAX_LENGTH}字符\";" +
+                "            };" +
+
+                "            var headers = this.getAllResponseHeaders();" +
+                "            if (headers.toLowerCase().includes(\"$TYPE_IMAGE\") || headers.toLowerCase().includes(\"$TYPE_VIDEO\")) {" +
+                "               this._isBinaryContent = true;" +
+                "            };" +
+                "            if (headers.length > $MAX_LENGTH) {" +
+                "                headers = headers.substring(0, $MAX_LENGTH) + \" 超出${MAX_LENGTH}字符\";" +
+                "            };" +
+
                 "            var requestBody = this._requestBody || '';" +
+                "            if (requestBody.length > $MAX_LENGTH) {" +
+                "                if (this._isBinaryContent) {" +
+                "                    requestBody = \"请求二进制文件参数, 超出${MAX_LENGTH}字符限制，不打印\";" +
+                "                } else {;" +
+                "                    requestBody = requestBody.substring(0, $MAX_LENGTH) + \" 超出${MAX_LENGTH}字符\";" +
+                "                };" +
+                "            };" +
+
+                "            var body = this.responseText;" +
+                "            if (body.length > $MAX_LENGTH) {" +
+                "                if (this._isBinaryContent) {" +
+                "                    body = \"二进制内容, 且超出${MAX_LENGTH}字符限制，不打印\";" +
+                "                } else {;" +
+                "                    body = body.substring(0, $MAX_LENGTH) + \" 超出${MAX_LENGTH}字符\";" +
+                "                };" +
+                "            };" +
+
                 "            var log = {" +
                 "                method: method," +
                 "                duration: duration," +
