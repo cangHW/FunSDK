c0feeaced32ee628f4740a06dc198f99fb6faf55
chenweiqi2 <chenweiqi2@tal.com>
2026-05-21 10:39:35 +0800

    优化kotlin委托

--- WebViewSDK/WebViewDSBridge/src/main/java/com/proxy/service/webview/bridge/ds/converter/WebServiceConverter.kt
+++ WebViewSDK/WebViewDSBridge/src/main/java/com/proxy/service/webview/bridge/ds/converter/WebServiceConverter.kt
@@ -3,7 +3,6 @@ package com.proxy.service.webview.bridge.ds.converter
 import com.proxy.service.webview.base.WebService
 import com.proxy.service.webview.base.config.WebConfig
 import com.proxy.service.webview.base.web.IWebLoader
-import com.proxy.service.webview.base.web.callback.ValueCallback
 import com.proxy.service.webview.bridge.ds.bridge.GlobalBridgeManager
 
 /**
@@ -11,7 +10,9 @@ import com.proxy.service.webview.bridge.ds.bridge.GlobalBridgeManager
  * @date: 2024/8/10 15:26
  * @desc:
  */
-class WebServiceConverter(private val service: WebService) : WebService {
+class WebServiceConverter(
+    private val service: WebService
+) : WebService by service{
 
     override fun addGlobalJavascriptInterface(any: Any) {
         service.addGlobalJavascriptInterface(any)
@@ -23,31 +24,8 @@ class WebServiceConverter(private val service: WebService) : WebService {
         GlobalBridgeManager.put(nameSpace, any)
     }
 
-    override fun setAcceptGlobalCookie(accept: Boolean) {
-        service.setAcceptGlobalCookie(accept)
-    }
-
-    override fun getGlobalCookie(url: String): String {
-        return service.getGlobalCookie(url)
-    }
-
-    override fun setGlobalCookie(url: String, value: String, callback: ValueCallback<Boolean>?) {
-        service.setGlobalCookie(url, value, callback)
-    }
-
-    override fun removeGlobalCookie(url: String, callback: ValueCallback<Boolean>?) {
-        service.removeGlobalCookie(url, callback)
-    }
-
-    override fun removeAllGlobalCookies(callback: ValueCallback<Boolean>?) {
-        service.removeAllGlobalCookies(callback)
-    }
-
     override fun createWebLoader(config: WebConfig): IWebLoader {
         return WebLoaderConverter(service.createWebLoader(config))
     }
 
-    override fun flushGlobalCookie() {
-        service.flushGlobalCookie()
-    }
 }
--- WebViewSDK/WebViewMonitor/src/main/java/com/proxy/service/webview/monitor/converter/WebServiceConverter.kt
+++ WebViewSDK/WebViewMonitor/src/main/java/com/proxy/service/webview/monitor/converter/WebServiceConverter.kt
@@ -3,48 +3,18 @@ package com.proxy.service.webview.monitor.converter
 import com.proxy.service.webview.base.WebService
 import com.proxy.service.webview.base.config.WebConfig
 import com.proxy.service.webview.base.web.IWebLoader
-import com.proxy.service.webview.base.web.callback.ValueCallback
 
 /**
  * @author: cangHX
  * @date: 2026/1/23 13:29
  * @desc:
  */
-class WebServiceConverter(private val service: WebService): WebService {
+class WebServiceConverter(
+    private val service: WebService
+): WebService by service{
 
     override fun createWebLoader(config: WebConfig): IWebLoader {
         return WebLoaderConverter(service.createWebLoader(config))
     }
 
-    override fun addGlobalJavascriptInterface(any: Any) {
-        service.addGlobalJavascriptInterface(any)
-    }
-
-    override fun addGlobalJavascriptInterface(nameSpace: String, any: Any) {
-        service.addGlobalJavascriptInterface(nameSpace, any)
-    }
-
-    override fun setAcceptGlobalCookie(accept: Boolean) {
-        service.setAcceptGlobalCookie(accept)
-    }
-
-    override fun getGlobalCookie(url: String): String {
-        return service.getGlobalCookie(url)
-    }
-
-    override fun setGlobalCookie(url: String, value: String, callback: ValueCallback<Boolean>?) {
-        service.setGlobalCookie(url, value, callback)
-    }
-
-    override fun removeGlobalCookie(url: String, callback: ValueCallback<Boolean>?) {
-        service.removeGlobalCookie(url, callback)
-    }
-
-    override fun removeAllGlobalCookies(callback: ValueCallback<Boolean>?) {
-        service.removeAllGlobalCookies(callback)
-    }
-
-    override fun flushGlobalCookie() {
-        service.flushGlobalCookie()
-    }
 }
