e86c9256ffd973245aa6c7a0f95f4f18a8f8b338
chenweiqi2 <chenweiqi2@tal.com>
2026-05-31 23:07:09 +0800

    更新

--- ApmSDK/ApmInfo/src/main/java/com/proxy/service/apm/info/CsApmMonitor.kt
+++ ApmSDK/ApmInfo/src/main/java/com/proxy/service/apm/info/CsApmMonitor.kt
@@ -5,9 +5,6 @@ import com.proxy.service.apm.info.config.ApmConfig
 /**
  * Apm 稳定性监控框架全局入口。
  *
- * 须在 [com.proxy.service.core.CsCore.init] 之前调用 [setConfig]，
- * 以便 [ApmMonitorConfig] 自动初始化时能读到业务配置（含主线程慢分发 [MainThreadLagConfig]）。
- *
  * @author: cangHX
  * @date: 2025/4/22 17:38
  */
